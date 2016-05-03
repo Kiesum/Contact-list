3cca5ff48f8ec7fe575e102a770d6bae9a3c6f20
Kiesum <kieran_summerfeldt@hotmail.com>
2016-05-03 19:34:29 +0000

    added list of contacts

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,16 @@
 # Homepage (Root path)
 get '/' do
+  @contacts = Contact.all
   erb :index
+end
+
+post '/' do 
+  @contact = Contact.new(
+    firstname: params[:firstname],
+    lastname: params[:lastname],
+    email: params[:email]
+    )
+
+  @contact.save 
+  redirect '/'
 end
